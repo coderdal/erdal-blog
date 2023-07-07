a6e557d3c25d33601b5da940a33d2187a938212a
coderdal <agircoder@gmail.com>
2023-07-07 10:06:22 +0300

    style changes

--- app/components/Home/HomeBanner.jsx
+++ app/components/Home/HomeBanner.jsx
@@ -2,18 +2,17 @@ import { GithubIcon, LinkedinIcon } from "@/app/components/Home/icons";
 
 const HomeBanner = () => {
   return (
-    <section className="p-6 lg:p-10 border rounded-md bg-blue-300/5 border-blue-100/10">
+    <section className="p-6 lg:p-10 border rounded-md  border-blue-100/10 max-xl:mx-3">
       <h1 className="text-3xl font-bold">Bloguma Hoşgeldin !</h1>
 
       <p className="text-md lg:text-lg text-gray-400 py-4">
-        Ben <strong>Muhammed ERDAL</strong>, Yazılım Geliştiricisiyim. Front-end
-        ve Back-end uygulamaları geliştirme konusunda deneyimliyim.
+        Ben <strong>Muhammed ERDAL</strong>, Yazılım Geliştiricisiyim.
         <br />
-        Bu Web sayfasında, kodlama ve faydalı bulduğum birçok içeriği barındıran
-        yazılar paylaşıyorum.
+        Bu Web sayfasında, Yazılım ve Faydalı Olabilecek birçok içeriği
+        barındıran yazılar paylaşıyorum.
       </p>
 
-      <div className="flex text-gray-300">
+      <div className="flex text-gray-300 max-sm:justify-center max-sm:mt-2">
         <a
           href="https://github.com/coderdal"
           className="mr-3 hover:text-white transition-colors"
