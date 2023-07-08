4afa485034149427ec7427dd9230cba23d1736ee
coderdal <agircoder@gmail.com>
2023-07-08 10:16:38 +0300

    style fixed

--- app/components/Blog/ListItem.jsx
+++ app/components/Blog/ListItem.jsx
@@ -6,9 +6,11 @@ const ListItem = ({ title, categories, date, slug }) => {
   return (
     <div className="my-1 py-2 px-4 rounded bg-zinc-950/10 hover:bg-zinc-950 transition-colors flex justify-between group">
       <Link href={`/blog/${slug}`} className="flex-1">
-        <h2 className="text-lg font-semibold">{title}</h2>
+        <h2 className="text-sm lg:text-lg group-hover:text-blue-400 transition-colors">
+          {title}
+        </h2>
       </Link>
-      <div className="opacity-0 group-hover:opacity-100">
+      <div className="opacity-0 group-hover:opacity-100 select-none">
         <div className="date text-gray-400">
           <span className="inline-block self-center mr-[6px]">
             <CalendarIcon size={14} />
