d2b358cde5863dc77d7314fdc6664d7b5fa90a01
coderdal <agircoder@gmail.com>
2023-07-08 10:13:56 +0300

    blog list page created

---
+++ app/blog/page.jsx
@@ -0,0 +1,41 @@
+import ListItem from "@/app/components/Blog/ListItem";
+
+async function getPosts() {
+  const res = await fetch("http://localhost:3000/api/posts");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const Blog = async () => {
+  const { posts } = await getPosts();
+
+  console.log(posts);
+
+  return (
+    <main className="w-full max-w-7xl mx-auto min-h-full  flex flex-col mt-4">
+      <section className="w-full max-w-7xl mx-auto py-5 lg:py-6 max-xl:px-6 max-sm:px-4">
+        <h1 className="font-extrabold text-4xl">Blog</h1>
+
+        <div className="mt-4 flex flex-col">
+          {posts.map((post, key) => {
+            return (
+              <ListItem
+                title={post.blog_title}
+                categories={post.blog_categories}
+                date={post.blog_date}
+                slug={post.blog_slug}
+                key={key}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Blog;
---
+++ app/components/Blog/ListItem.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+import { CalendarIcon } from "@/app/components/Home/icons";
+
+const ListItem = ({ title, categories, date, slug }) => {
+  return (
+    <div className="my-1 py-2 px-4 rounded bg-zinc-950/10 hover:bg-zinc-950 transition-colors flex justify-between group">
+      <Link href={`/blog/${slug}`} className="flex-1">
+        <h2 className="text-lg font-semibold">{title}</h2>
+      </Link>
+      <div className="opacity-0 group-hover:opacity-100">
+        <div className="date text-gray-400">
+          <span className="inline-block self-center mr-[6px]">
+            <CalendarIcon size={14} />
+          </span>
+          <time className="text-sm">{date}</time>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListItem;
