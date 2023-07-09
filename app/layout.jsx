cfecf6ab6e26543cf766fadc86d2573e2005eed5
coderdal <agircoder@gmail.com>
2023-07-09 12:13:45 +0300

    top loader added

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import Header from "@/app/components/Header/Header";
 import Footer from "@/app/components/Footer/Footer";
 
+import NextTopLoader from "nextjs-toploader";
+
 export const metadata = {
   title: "Muhammed ERDAL | Blog",
   description: "Blog of a Frontend Developer",
@@ -12,6 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="overflow-y-scroll">
+        <NextTopLoader
+          color="#074163"
+          height={2}
+          speed={400}
+          shadow="0 0 10px #2299DD,0 0 5px #10537a"
+        />
         <Header />
         {children}
         <Footer />
